bab4a3322b56d0d30e2a52098759bb7cf5d689aa
KooTaehyeon <vcro12123@naver.com>
2025-01-07 17:03:02 +0900

    socket 수정2

--- src/util/socket_noti.ts
+++ src/util/socket_noti.ts
@@ -1,9 +1,9 @@
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
 
-const socket = io("http://localhost:4000"); // WebSocket 서버 URL 확인
+const socket = io('https://celebright.p-e.kr/api'); // WebSocket 서버 URL 확인
 
-socket.on("new_notification", (data) => {
-  console.log("New notification received:", data);
+socket.on('new_notification', (data) => {
+  console.log('New notification received:', data);
   // 알림을 처리하는 로직 추가
 });
 
