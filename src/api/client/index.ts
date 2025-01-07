9a3f189d6081bd6606833a776779d3d8bd6c31f3
KooTaehyeon <vcro12123@naver.com>
2025-01-07 14:30:10 +0900

    axiosInstance 수정

--- src/api/client/index.ts
+++ src/api/client/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL = process.env.REACT_APP_BACKEND_URL;
 
 const axiosInstance = axios.create({
-  baseURL: baseURL,
+  baseURL: baseURL + '/api',
   withCredentials: true,
 });
 
