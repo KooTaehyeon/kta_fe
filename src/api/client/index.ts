47a93a825be504b4a57e1bd80898c5f78fd7b9ec
KooTaehyeon <vcro12123@naver.com>
2025-01-09 16:20:24 +0900

    baseUrl 재수정

--- src/api/client/index.ts
+++ src/api/client/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL = process.env.REACT_APP_BACKEND_URL;
 
 const axiosInstance = axios.create({
-  baseURL: baseURL + '/api',
+  baseURL: baseURL,
   withCredentials: true,
 });
 
